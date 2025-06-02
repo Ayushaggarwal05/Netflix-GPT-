d5ec113d8ca9d0fb65178069f2e61d6fcb9d6f18
Ayush aggarwal <aayushaggarwal348@gmail.com>
2025-06-02 17:07:21 +0530

    Creating my Browse page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,6 +3,7 @@ import { useClerk, useUser } from "@clerk/clerk-react";
 import { useNavigate } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import { removeUser } from "../utils/userSlice";
+import { LOGO } from "../utils/constant";
 
 const Header = () => {
   const { signOut } = useClerk();
@@ -26,11 +27,7 @@ const Header = () => {
 
   return (
     <div className="absolute w-screen px-8 py-2 bg-gradient-to-b from-black z-10 flex justify-between">
-      <img
-        className="w-46 "
-        src="https://help.nflxext.com/helpcenter/OneTrust/oneTrust_production/consent/87b6a5c0-0104-4e96-a291-092c11350111/01938dc4-59b3-7bbc-b635-c4131030e85f/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png"
-        alt="logo"
-      />
+      <img className="w-46 " src={LOGO} alt="logo" />
 
       {isUserSignedIn && (
         <div className="flex p-2">
---
+++ src/components/VideoTitle.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const VideoTitle = ({ title, overview }) => {
+  return (
+    <div className="pt-38 px-12">
+      <h1 className="text-6xl font-bold">{title}</h1>
+      <p className="py-6 text-lg w-1/4">{overview}</p>
+      <div>
+        <button className="bg-gray-500 text-white p-4 px-10 text-xl bg-opacity-50 rounded-lg font-bold">
+          ▶️Play
+        </button>
+        <button className="bg-gray-500 mx-2 text-white p-4 px-10 text-xl bg-opacity-50 rounded-lg font-bold">
+          ℹ️More Info
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default VideoTitle;
