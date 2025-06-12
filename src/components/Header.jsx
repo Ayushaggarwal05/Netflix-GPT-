b689ca18aa6a00dfe9650a3f03d7e36461ad6bcb
Ayush aggarwal <aayushaggarwal348@gmail.com>
2025-06-12 16:42:24 +0530

    Creating gpt search

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,6 +4,7 @@ import { useNavigate } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import { removeUser } from "../utils/userSlice";
 import { LOGO } from "../utils/constant";
+import { toggleGptSearchView } from "../utils/gptSlice";
 
 const Header = () => {
   const { signOut } = useClerk();
@@ -22,6 +23,11 @@ const Header = () => {
     }
   };
 
+  const handleGptSearchClick = () => {
+    //toggle my GPT search
+    dispatch(toggleGptSearchView());
+  };
+
   // Check if user is signed in (either Clerk user or Redux user exists)
   const isUserSignedIn = clerkUser || reduxUser;
 
@@ -31,6 +37,11 @@ const Header = () => {
 
       {isUserSignedIn && (
         <div className="flex p-2">
+          <button
+            className="py-2 cursor-pointer px-4 mx-4 my-2 mt-0 bg-purple-800 text-white rounded-lg"
+            onClick={handleGptSearchClick}>
+            GPT Search
+          </button>
           <img
             src="https://wallpapers.com/images/hd/netflix-profile-pictures-1000-x-1000-qo9h82134t9nv0j0.jpg"
             alt="usericon_logo"
